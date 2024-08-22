2837647d79b9e90787af8e435301d25b6af8929c
makarovri <144092099+makarovri@users.noreply.github.com>
2024-08-22 09:40:36 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,17 +1,17 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-from clickhouse_driver import Client
-import os
+#from clickhouse_driver import Client
+#import os
 
-CLICK_HOUSE_HOST = os.environ.get('CLICK_HOUSE_HOST')
-CLICK_HOUSE_USER = os.environ.get('CLICK_HOUSE_USER')
-CLICK_HOUSE_PASSWORD = os.environ.get('CLICK_HOUSE_PASSWORD')
-CLICK_HOUSE_DB = os.environ.get('CLICK_HOUSE_DB')
+#CLICK_HOUSE_HOST = os.environ.get('CLICK_HOUSE_HOST')
+#CLICK_HOUSE_USER = os.environ.get('CLICK_HOUSE_USER')
+#CLICK_HOUSE_PASSWORD = os.environ.get('CLICK_HOUSE_PASSWORD')
+#CLICK_HOUSE_DB = os.environ.get('CLICK_HOUSE_DB')
 
-client = Client(CLICK_HOUSE_HOST,
-                user=CLICK_HOUSE_USER,
-                password=CLICK_HOUSE_PASSWORD,
-                database=CLICK_HOUSE_DB) # подключение к ClickHouse
+#client = Client(CLICK_HOUSE_HOST,
+#                user=CLICK_HOUSE_USER,
+#                password=CLICK_HOUSE_PASSWORD,
+#                database=CLICK_HOUSE_DB) # подключение к ClickHouse
 
 app = FastAPI()
 
@@ -36,12 +36,13 @@ def parse_data(data):
 @app.post("/")
 async def insert_data(item: Item):
     try:
+         print(item.data)
         # SQL-запрос для вставки данных
-        cnt = client.execute("INSERT INTO raw_data (*) VALUES",
-                       parse_data(item.data))
+#        cnt = client.execute("INSERT INTO raw_data (*) VALUES",
+#                       parse_data(item.data))
         return {
             "message": "Item added successfully",
-            "count": cnt
+#            "count": cnt
         }
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error inserting item: {e}")
