3337890e2cbbbdedd816b7f47e62c778eedade1e
makarovri <144092099+makarovri@users.noreply.github.com>
2024-08-22 14:32:25 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,12 +1,14 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Request
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
 
 @app.post("/")
-async def insert_data(item):
+async def root(request: Request):
     try:
-        print(item)
-        return {"item": item}
+        body = await request.body()
+        print(body.decode())
+        return JSONResponse(content={"request_body": body.decode()})
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error inserting item: {e}")
