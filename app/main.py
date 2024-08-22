8384328207c6235ca48b3bc2032cc841096d2dfe
makarovri <144092099+makarovri@users.noreply.github.com>
2024-08-22 16:53:49 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,5 +1,11 @@
 from fastapi import FastAPI, HTTPException, Request
+from clickhouse_driver import Client
 from fastapi.responses import JSONResponse
+
+client = Client(clickhouse,
+                user='default',
+                password='',
+                database='arnavi') # подключение к ClickHouse
 
 app = FastAPI()
 
@@ -7,7 +13,15 @@ app = FastAPI()
 @app.post("/")
 async def root(request: list):
     try:
-        print(body.decode())
+        # print(body)
+
+        json_data = json.loads(body)
+
+        for i in json_data:
+            # SQL-запрос для вставки данных
+            cnt = client.execute(f"INSERT INTO flespi_daata {tuple(i.keys())} VALUES", i.values())
+        
         return JSONResponse(content={"request_body": body})
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error inserting item: {e}")
+        
